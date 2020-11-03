477d6ea97324177b868ff6834c2e2c3ec0f7b520
qbart <wojtowicz.bartlomiej@gmail.com>
2020-11-03 01:04:29 +0100

    qbart: Add clipboard button

--- keyboards/ergodox_ez/keymaps/qbart/keymap.c
+++ keyboards/ergodox_ez/keymaps/qbart/keymap.c
@@ -23,21 +23,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB         ,KC_Q           ,KC_W           ,KC_E           ,KC_R           ,KC_T           ,KC_LCBR,
 LT(L_SYM, KC_QUOT) ,KC_A           ,KC_S           ,KC_D           ,KC_F           ,KC_G           ,
     KC_DQT         ,LCTL_T(KC_Z)   ,KC_X           ,KC_C           ,KC_V           ,KC_B           ,KC_LBRC,
-LOPT_T(KC_DEL)     ,KC_EXLM        ,KC_PIPE        ,KC_AMPR        ,KC_LSPO             ,
+    KC_RALT        ,KC_EXLM        ,KC_PIPE        ,KC_AMPR        ,KC_LSPO             ,
     // left thumb --------------------------------------------------------------------------------------------------
                                                                                    KC_DEL,          KC_F5,
-                                                                                                    KC_1,
+                                                                                                    SGUI(KC_V),
                                                                     KC_SPC,      TD(_WIN),          KC_ESC,
     // right hand --------------------------------------------------------------------------------------------------
     KC_AT          ,KC_6           ,KC_7           ,KC_8           ,KC_9           ,KC_0           ,KC_BSPC,
     KC_RCBR        ,KC_Y           ,KC_U           ,KC_I           ,KC_O           ,KC_P           ,KC_ASTR,
                     KC_H           ,KC_J           ,KC_K           ,KC_L           ,KC_SCLN        ,KC_EQL,
     KC_RBRC        ,KC_N           ,KC_M           ,KC_COMMA       ,KC_DOT         ,RCTL_T(KC_SLSH),KC_MINUS,
-                                    KC_RSPC        ,KC_DLR         ,KC_RALT        ,KC_LEAD        ,TO(L_NAV),
+                                    KC_RSPC        ,KC_DLR         ,XXXXXXX        ,KC_LEAD        ,TO(L_NAV),
     // right thumb -------------------------------------------------------------------------------------------------
     KC_CAPS,        KC_BSLS,
-    KC_3,
-    KC_RALT,           KC_ESC,        KC_ENT
+    XXXXXXX,
+    XXXXXXX,           KC_ESC,        KC_ENT
 ),
 
 [L_SYM] = LAYOUT_ergodox(
