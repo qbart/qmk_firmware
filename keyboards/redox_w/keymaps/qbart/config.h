e980d229679e77fb89512aba25bbd3b43b7b7236
Bartłomiej Wójtowicz <wojtowicz.bartlomiej@gmail.com>
2020-11-23 14:21:06 +0100

    qbart: Fix tapping term (redox)

--- keyboards/redox_w/keymaps/qbart/config.h
+++ keyboards/redox_w/keymaps/qbart/config.h
@@ -1,6 +1,4 @@
 #pragma once
 
-/* https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_leader_key */
-#define LEADER_PER_KEY_TIMING
-#define LEADER_TIMEOUT 250
+#undef TAPPING_TERM
 #define TAPPING_TERM 250
